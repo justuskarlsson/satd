6ac38c657c7c15ade26701d948ca770df92ddd4b
Justus Karlsson <karlssonjustus@gmail.com>
2024-02-17 02:38:10 +0100

    Good preview gui

--- satd/explore.py
+++ satd/explore.py
@@ -1,21 +1,94 @@
 import dearpygui.dearpygui as dpg
+import numpy as np
+from PIL import Image
+from io import BytesIO
+import requests
 
-from satd.search import search
+from satd.search import search, Feature
 
-def search_callback():
-    collection = search()
-    print(collection)
+WIDTH = 1920
+HEIGHT = 1080
+
+PREVIEW_WIDTH = 512
+PREVIEW_HEIGHT = 512
+
+
+
 
 dpg.create_context()
-dpg.create_viewport()
-dpg.setup_dearpygui()
 
-with dpg.window(label="Example Window"):
-    dpg.add_text("Hello world")
-    dpg.add_input_text(label="string")
-    dpg.add_slider_float(label="float")
+class SearchVis:
+    features: list[Feature] = []
+    imgs: list[np.ndarray] = []
+    idx: int = 0
+
+    def set_collection(self, features: list[Feature]):
+        self.features = features
+        self.features.sort(reverse=True)
+        self.imgs = [None] * len(features)
+        self.load()
+
+    def load(self):
+        if self.idx > len(self.features) - 1:
+            return
+        feature = self.features[self.idx]
+        dpg.set_value("idx", f"{self.idx + 1:02d}/{len(self.features):02d}")
+        dpg.set_value("date", feature.datetime)
+        if self.imgs[self.idx] is None:
+            print("Fetching image...")
+            response = requests.get(feature.quicklook_href)
+            img = Image.open(BytesIO(response.content))
+            print("Done.")
+            img = img.resize((PREVIEW_WIDTH, PREVIEW_HEIGHT))
+            rgba = np.ones((PREVIEW_HEIGHT, PREVIEW_WIDTH, 4), np.float32)
+            rgb = np.array(img, dtype=np.float32) / 255.0
+            rgba[..., :3] = rgb
+            self.imgs[self.idx] = rgba
+        dpg.set_value("texture", self.imgs[self.idx])
+
+
+    def prev(self):
+        self.idx = max(0, self.idx - 1)
+        self.load()
+
+    def next(self):
+        self.idx = min(len(self.features) - 1, self.idx + 1)
+        self.load()
+
+search_vis = SearchVis()
+
+def search_callback():
+    lkpg = [15.618669973441541, 58.40504766611348, 15.628669973441541, 58.41504766611348]
+    collection = search(bbox=lkpg, time_range="2023-12-01/2024-02-17")
+    search_vis.set_collection(collection.features)
+
+
+
+with dpg.texture_registry(show=False):
+    buffer = np.ones((PREVIEW_HEIGHT, PREVIEW_WIDTH, 4), dtype=np.float32)
+    dpg.add_raw_texture(
+        width=PREVIEW_WIDTH,
+        height=PREVIEW_HEIGHT,
+        default_value=buffer,
+        format=dpg.mvFormat_Float_rgba,
+        tag="texture",
+    )
+
+
+with dpg.window(label="Preview", width=PREVIEW_WIDTH, height=PREVIEW_HEIGHT, pos=(WIDTH-PREVIEW_WIDTH, 0)):
+    dpg.add_text("0/0", tag="idx")
+    dpg.add_text("", tag="date")
+    dpg.add_image("texture")
+
+with dpg.window(label="Search", width=300, height=HEIGHT, pos=(0, 0)):
     dpg.add_button(label="Search", callback=search_callback)
 
+with dpg.handler_registry():
+    dpg.add_key_press_handler(dpg.mvKey_M, callback=search_vis.prev)
+    dpg.add_key_press_handler(dpg.mvKey_N, callback=search_vis.next)
+
+dpg.create_viewport(width=WIDTH, height=HEIGHT, resizable=False)
+dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.start_dearpygui()
 dpg.destroy_context()
--- satd/search.py
+++ satd/search.py
@@ -2,8 +2,10 @@
 
 import requests
 import json
+
 # Define the correct API endpoint for STAC
 stac_endpoint = "https://catalogue.dataspace.copernicus.eu/stac/collections"
+
 
 class Feature:
     def __init__(self, data):
@@ -12,49 +14,56 @@ class Feature:
     @property
     def properties(self):
         return self.data["properties"]
+    
+    @property
+    def datetime(self):
+        return self.properties['datetime']
 
     @property
     def quicklook_href(self):
         if "QUICKLOOK" not in self.data["assets"]:
             return ""
         return self.data["assets"]["QUICKLOOK"]["href"]
-    
+
     @property
     def product(self):
         return self.data["assets"]["PRODUCT"]
-    
+
     @property
     def product_s3_href(self):
         return self.product["alternate"]["s3"]["href"]
-    
+
     def __str__(self):
-        return f"({self.properties['datetime']}) thumbnail: {self.quicklook_href}"
+        return f"({self.datetime}) thumbnail: {self.quicklook_href}"
+    
+    def __lt__(self, other):
+        return self.datetime < other.datetime
+
 
 class FeatureCollection:
     def __init__(self, data):
         self.data = data
-        self.features = [
-            Feature(f) for f in self.data["features"]
-        ]
-    
+        self.features = [Feature(f) for f in self.data["features"]]
+
     def __len__(self):
         return len(self.features)
-    
+
     def __getitem__(self, i) -> Feature:
         return self.features[i]
-    
+
     def __str__(self):
         return "\n".join([str(f) for f in self.features])
 
-def search(bbox=[12.41, 41.80, 12.52, 41.90], time_range="2022-01-01/2022-03-01", sensor="SENTINEL-2"):
 
-    
+def search(
+    bbox=[12.41, 41.80, 12.52, 41.90],
+    time_range="2022-01-01/2022-03-01",
+    limit=100,
+    sensor="SENTINEL-2",
+):
 
     # Construct the search query
-    search_params = {
-        "bbox": ",".join([str(x) for x in bbox]),
-        "datetime": time_range
-    }
+    search_params = {"bbox": ",".join([str(x) for x in bbox]), "datetime": time_range, "limit": limit}
 
     url = f"{stac_endpoint}/{sensor}/items?"
 
@@ -65,9 +74,10 @@ def search(bbox=[12.41, 41.80, 12.52, 41.90], time_range="2022-01-01/2022-03-01"
         print("Failed to fetch data:", response.status_code)
         return []
 
-    collection =  FeatureCollection(response.json())
+    collection = FeatureCollection(response.json())
 
     return collection
+
 
 if __name__ == "__main__":
     collection = search()
